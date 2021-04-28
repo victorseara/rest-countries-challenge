37e429fbda8427cb698c66a87394e754ab8bb0ff
victorseara <victorseara.me@gmail.com>
2021-04-28 00:23:14 -0300

    chore(config): :heavy_plus_sign: added and configured eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
+  const [state, setState] = React.useState(false);
+
+  function update() {
+    return setState((prevstate) => !prevstate);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>Welcome 👋</p>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
