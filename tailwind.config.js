16e8f8ecdc036d7c646c10dba7188f9a70519f2d
victorseara <victorseara.me@gmail.com>
2021-04-28 22:12:45 -0300

    chore: :lipstick: configured app theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: ['./src/**/*.{ts,tsx}', './public/index.html'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Nunito Sans', ...defaultTheme.fontFamily.sans],
+      },
+      animation: {
+        'spin-demo': 'spin 20s linear infinite',
+      },
+      colors: {
+        'common-blue': 'hsl(209, 23%, 22%)',
+        'dark-blue': 'hsl(207, 26%, 17%)',
+        'very-dark-blue': 'hsl(200, 15%, 8%)',
+        'dark-gray': 'hsl(0, 0%, 52%)',
+        'light-gray': 'hsl(0, 0%, 98%)',
+        white: 'hsl(0, 0%, 100%)',
+        ...defaultTheme.colors,
+      },
+    },
   },
   variants: {
     extend: {},
