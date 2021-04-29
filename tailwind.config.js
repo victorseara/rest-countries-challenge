a98501d976b1cc4acef15eaa8712a55622e92056
victorseara <victorseara.me@gmail.com>
2021-04-28 21:18:33 -0300

    chore: :heavy_plus_sign: configured tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/**/*.{ts,tsx}', './public/index.html'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
