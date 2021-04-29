9605b870ba64041050277ea871dc3d00cc71feba
victorseara <victorseara.me@gmail.com>
2021-04-28 22:14:06 -0300

    refactor: replaced App.css with tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,49 @@
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+  useEffect(() => {
+    const htmlEl = document.querySelector('html');
+    if (!isDarkMode) {
+      htmlEl?.classList.remove('dark');
+      htmlEl?.classList.add('light');
+    } else {
+      htmlEl?.classList.remove('light');
+      htmlEl?.classList.add('dark');
+    }
+  }, [isDarkMode]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="text-center h-screen dark:bg-dark-blue">
+      <header className="h-full flex flex-col items-center justify-center text-2xl dark:text-white">
+        <img
+          src={logo}
+          className="h-1/3 pointer-events-none animate-spin-demo"
+          alt="logo"
+        />
         <p>Welcome 👋</p>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="dark:text-yellow-50"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+        <label htmlFor="theme" className="my-4">
+          <input
+            type="checkbox"
+            checked={isDarkMode}
+            onChange={e => setIsDarkMode(e.target.checked)}
+            className="mr-2"
+          />
+          Dark mode
+        </label>
       </header>
     </div>
   );
