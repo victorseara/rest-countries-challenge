39c8602c267ec2dc3605f680d25a770c413867ca
victorseara <victorseara.me@gmail.com>
2021-04-30 00:45:11 -0300

    style: :lipstick: fix visibility on dark mode button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,14 +9,14 @@ const Header = ({ title, toggleTheme }: HeaderProps) => {
   return (
     <div className="w-full bg-white dark:bg-common-blue py-6 px-4 shadow-md flex justify-center mb-16 text-dark-blue dark:text-white">
       <div className="w-10/12 max-w-screen-2xl flex justify-between items-center">
-        <h1 className="font-bold text-3xl">{title}</h1>
+        <h1 className="font-bold text-xl sm:text-3xl">{title}</h1>
         <button
           type="button"
           className="flex cursor-pointer hover:opacity-80"
           onClick={toggleTheme}
         >
           <Moon />
-          <span className="ml-2 font-semibold">Dark mode</span>
+          <span className="ml-2 font-semibold hidden sm:inline">Dark mode</span>
         </button>
       </div>
     </div>
