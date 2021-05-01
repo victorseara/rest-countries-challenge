13f14365f60f837a4a52bda6a0e35e96c8e08e16
victorseara <victorseara.me@gmail.com>
2021-04-30 21:27:25 -0300

    feat(COUNTRIES-7): :construction: added CountryCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,62 @@
+import { useEffect, useState } from 'react';
+import Card, { CountryGeneralInformation } from './components/Card/Card';
+import Dropdown from './components/Dropdown/Dropdown';
+import Header from './components/Header/Header';
 import SearchBox from './components/SearchBox/SearchBox';
+import mock from './components/Card/mock.json';
+
+const countries = mock as CountryGeneralInformation[];
+
+type UserTheme = 'light' | 'dark';
 
 function App() {
+  const [userTheme, setUserTheme] = useState<UserTheme>('light');
+
+  localStorage.setItem('theme', 'dark');
+
+  useEffect(() => {
+    const theme = localStorage.getItem('theme') as UserTheme;
+
+    if (!theme) {
+      localStorage.setItem('theme', 'light');
+    }
+
+    if (theme === 'dark') {
+      setUserTheme('dark');
+      document.querySelector('html')?.classList.add('dark');
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    if (userTheme === 'dark') {
+      setUserTheme('light');
+      document.querySelector('html')?.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+    } else {
+      setUserTheme('dark');
+      document.querySelector('html')?.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+    }
+  };
+
   return (
-    <div className="h-full py-12">
-      <SearchBox />
+    <div className="h-full flex flex-col items-center overflow-x-hidden">
+      <Header title="Where in the world ?" toggleTheme={toggleTheme} />
+      <div className="h-full w-10/12 max-w-screen-2xl flex flex-col">
+        <div className="flex flex-col sm:justify-between sm:flex-row">
+          <div className="w-12/12 sm:w-1/2">
+            <SearchBox placeholder="Search for a country..." />
+          </div>
+          <div className="mt-12 w-8/12 sm:mt-0 sm:w-2/12">
+            <Dropdown />
+          </div>
+        </div>
+        <main className="mt-12 flex-1 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full gap-20">
+          {countries.map(country => (
+            <Card country={country} key={country.name} />
+          ))}
+        </main>
+      </div>
     </div>
   );
 }
---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,40 @@
+export interface CountryGeneralInformation {
+  flag: string;
+  name: string;
+  capital: string;
+  region: string;
+  population: number;
+}
+
+interface CardProps {
+  country: CountryGeneralInformation;
+}
+
+const Card = ({ country }: CardProps) => {
+  return (
+    <div className="flex flex-col dark:bg-common-blue rounded-md shadow-md dark:text-white text-very-dark-blue min-w-min">
+      <img
+        src={country.flag}
+        alt={`Flag of ${country.name}`}
+        className="object-cover w-full h-52 rounded-t-md"
+      />
+      <div className="py-8 px-6">
+        <h3 className="font-bold text-lg mb-4">{country.name}</h3>
+        <ul>
+          <li className="mb-2">
+            <span className="font-semibold">Population: </span>{' '}
+            {country.population.toLocaleString()}
+          </li>
+          <li className="mb-2">
+            <span className="font-semibold">Region: </span> {country.region}
+          </li>
+          <li className="mb-2">
+            <span className="font-semibold">Capital: </span> {country.capital}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
