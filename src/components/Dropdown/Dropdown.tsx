f774d2660796ef5ba9138f1972d20b8e5621999d
victorseara <victorseara.me@gmail.com>
2021-05-01 15:50:40 -0300

    refactor: added change feature to Dropdown

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -5,23 +5,31 @@ type Options = string[];
 interface DropdownProps {
   name?: string;
   placeholder?: string;
+  value?: string;
   onChange?: (event: React.ChangeEvent<HTMLSelectElement>) => void;
   options: Options;
 }
 
-const Dropdown = ({ name, placeholder, onChange, options }: DropdownProps) => {
+const Dropdown = ({
+  name,
+  placeholder,
+  onChange,
+  options,
+  value,
+}: DropdownProps) => {
   return (
     <label
       htmlFor="Filter by Region"
-      className="relative flex items-center text-gray-500 dark:text-gray-200 h-14 rounded-md shadow-md"
+      className="relative flex items-center text-gray-500 dark:text-gray-200 h-full rounded-md shadow-md"
     >
       <ChevronDown className="absolute my-auto right-0 mx-4" />
       <select
         onChange={onChange}
         name={name}
+        value={value}
         className="w-full h-full dark:bg-common-blue px-8 bg-white rounded-md font-semibold focus:outline-none focus:ring-inset focus:ring-dark-hover focus:ring-1"
       >
-        <option selected disabled hidden className="font-semibold">
+        <option disabled hidden className="font-semibold" value="">
           {placeholder || 'Select an option'}
         </option>
         {options.map(item => (
