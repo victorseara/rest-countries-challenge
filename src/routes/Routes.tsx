c49a6e244416d2dea141013ec091af5c85ae0ee8
victorseara <victorseara.me@gmail.com>
2021-05-04 00:22:44 -0300

    perf: :zap: code splitted to have faster initial loading

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,8 +1,11 @@
-import { useEffect, useState } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Header from '../components/Header/Header';
-import CountryDetails from '../pages/CountryDetails/CountryDetails';
-import Home from '../pages/Home/Home';
+
+const Home = React.lazy(() => import('pages/Home/Home'));
+const CountryDetails = React.lazy(
+  () => import('pages/CountryDetails/CountryDetails')
+);
 
 type UserTheme = 'light' | 'dark';
 
@@ -39,8 +42,10 @@ const Routes = () => {
         <Header title="Where in the world ?" toggleTheme={toggleTheme} />
         <div className="h-full w-10/12 max-w-screen-2xl flex flex-col self-center mt-20">
           <main className="h-full">
-            <Route path="/" exact component={Home} />
-            <Route path="/:countryCode" component={CountryDetails} />
+            <Suspense fallback={<span>Loading...</span>}>
+              <Route path="/" exact component={Home} />
+              <Route path="/:countryCode" component={CountryDetails} />
+            </Suspense>
           </main>
         </div>
       </div>
