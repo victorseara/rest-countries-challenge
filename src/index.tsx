d0e038f1d37dc7474271d9a0f4d9df52bf034bd0
victorseara <victorseara.me@gmail.com>
2022-07-05 01:30:36 -0300

    fix: fix build error

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-if (process.env.NODE_ENV === 'development') {
-  document.querySelector('body')?.classList.add('debug-screens');
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const { worker } = require('./mocks/browser');
+if (process.env.NODE_ENV === "development") {
+  document.querySelector("body")?.classList.add("debug-screens");
+  const { worker } = require("./mocks/browser");
   worker.start();
 }
 
-const el = document.getElementById('root');
+const el = document.getElementById("root");
 const root = createRoot(el as Element);
 
 root.render(
